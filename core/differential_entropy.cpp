2aab5385a79a6de340bbc85b0cd8d080ad01f4b6
Kaba <Kaba@localhost>
2009-05-29 04:23:43 +0300

    Added documentation.

--- core/differential_entropy.cpp
+++ core/differential_entropy.cpp
@@ -17,6 +17,22 @@ namespace Tim
 	{
 		PENSURE1(supportVolume >= 0, supportVolume);
 
+		/*
+		Let X be a random variable in R^n 
+		with a probability density function p.
+		Let S subset R^n be the support set of p.
+		The measure of S is m(S). Then because
+		of uniformity p(x) = 1 / m(S).
+		Now
+		H(X) = -int_S p(x) ln(p(x)) dx
+		= -int_S (1 / m(s)) ln(1 / m(S)) dx
+		= -int_S (1 / m(s)) (ln(1) - ln(m(S))) dx
+		= -int_S (1 / m(s)) (-ln(m(S))) dx
+		= (ln(m(S)) / m(S)) int_S 1 dx
+		= (ln(m(S)) / m(S)) m(S)
+		= ln(m(S))
+		*/
+
 		return std::log(supportVolume);
 	}
 
