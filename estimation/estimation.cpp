62226efefa454b451613b12a1994986d67554ac7
Kalle Rutanen <kaba@hilvi.org>
2009-06-05 02:40:43 +0300

    Added config file support for nearest neighbor performance testing.

--- estimation/estimation.cpp
+++ estimation/estimation.cpp
@@ -1,2 +1,26 @@
 #include "estimation.h"
 
+#include <pastel/device/devicesystem.h>
+
+#include <pastel/sys/log_all.h>
+
+#include <iostream>
+
+using namespace Pastel;
+using namespace Tim;
+
+void estimation();
+
+int estimationMain()
+{
+	deviceSystem().initialize();
+
+	//log().addObserver(LogObserverPtr(new StreamLogObserver(&std::cout)));
+	log().addObserver(LogObserverPtr(new FileLogObserver("log.txt")));
+
+	timTestList().console();
+
+	deviceSystem().deInitialize();
+
+	return 0;
+}
--- estimation/estimation_main.cpp
+++ estimation/estimation_main.cpp
@@ -1,23 +1,6 @@
-#include "estimation.h"
-
-#include <pastel/sys/log_all.h>
-
-#include <iostream>
-
-using namespace Pastel;
-using namespace Tim;
-
-
-void estimation();
+int estimationMain();
 
 int main()
 {
-	log().addObserver(LogObserverPtr(new StreamLogObserver(&std::cout)));
-	log().addObserver(LogObserverPtr(new FileLogObserver("log.txt")));
-
-	timTestList().console();
-
-	//estimation();
-
-	return 0;
+	return estimationMain();
 }
