3a6732d6bfa639ed4055ea5dcc7ec646cd3452f4
Kalle Rutanen <kaba@hilvi.org>
2009-07-09 14:25:06 +0300

    Added proper delay-embedding. Changed Signal testing to use a proper TestSuite and added some tests.

--- core/embed.cpp
+++ core/embed.cpp
@@ -5,29 +5,106 @@ namespace Tim
 {
 
 	TIMCORE SignalPtr delayEmbed(
-		const std::vector<real>& data,
-		integer dimension,
+		const SignalPtr& signal,
+		integer k,
+		integer shift,
 		integer step)
 	{
-		ENSURE1(dimension > 0, dimension);
+		ENSURE1(k > 0, k);
+		ENSURE1(shift >= 0, shift);
 		ENSURE1(step >= 1, step);
 
-		const integer stride = dimension * step;
+		// Given is a signal S : Z -> R^n.
+		// Form a signal R : Z -> R^n : 
+		// R(t) = (S(t0 + t), S(t0 + t + dt), ..., S(t0 + t + dt * (k - 1)).
+		//
+		// Then R is the delay-embedding and
+		// t0 is the embedding shift
+		// dt is the embedding delay
+		// k is the 'embedding factor'
+		// d = k n is the embedding dimension
 
-		const integer samples = 
-			(data.size() + (stride - 1)) / stride;
+		const integer n = signal->dimension();
 
-		SignalPtr signal = SignalPtr(new Signal(dimension, samples));
+		const integer embedDimension = k * n;
+		const integer embedSampleWidth = (k - 1) * step + 1;
+		const integer embedSamples = (signal->samples() - shift) - embedSampleWidth + 1;
 
-		for (integer i = 0;i < dimension;++i)
+		const SignalPtr embedSignal = SignalPtr(
+			new Signal(embedDimension, embedSamples));
+
+		integer sBegin = shift;
+		for (integer t = 0;t < embedSamples;++t)
 		{
-			for (integer j = 0;j < samples;++j)
+			integer iBegin = 0;
+			integer s = sBegin;
+			for (integer j = 0;j < k;++j)
 			{
-				signal->data()(i, j) = data[(i * samples + j) * step];
+				std::copy(
+					signal->data().columnBegin(s),
+					signal->data().columnEnd(s),
+					embedSignal->data().columnBegin(t) + iBegin);
+
+				s += step;
+				iBegin += n;
 			}
+			++sBegin;
 		}
 
-		return signal;
+		return embedSignal;
 	}
+
+	/*
+	TIMCORE SignalPtr delayEmbed(
+		const SignalPtr& signal,
+		integer k,
+		integer shift,
+		integer step)
+	{
+		ENSURE1(k > 0, k);
+		ENSURE1(shift >= 0, shift);
+		ENSURE1(step >= 1, step);
+
+		// Given is a signal S : Z -> R^n.
+		// Form a signal R : Z -> R^n : R(t) = S(t0 + dt * t).
+		// Form a signal Q : Z -> R^d : Q(t) = (R(t), R(t + 1), ..., R(t + k - 1)).
+		//
+		// Then Q is the delay-embedding and
+		// t0 is the embedding shift
+		// dt is the embedding delay
+		// k is the 'embedding factor'
+		// d = k n is the embedding dimension
+
+		const integer n = signal->dimension();
+
+		const integer embedDimension = k * n;
+		const integer culledSamples =
+			((signal->samples() - shift) + (step - 1)) / step;
+		const integer embedSamples = culledSamples - k + 1;
+
+		const SignalPtr embedSignal = SignalPtr(
+			new Signal(embedDimension, embedSamples));
+
+		integer sBegin = shift;
+		for (integer t = 0;t < embedSamples;++t)
+		{
+			integer iBegin = 0;
+			integer s = sBegin;
+			for (integer j = 0;j < k;++j)
+			{
+				std::copy(
+					signal->data().columnBegin(s),
+					signal->data().columnEnd(s),
+					embedSignal->data().columnBegin(t) + iBegin);
+
+				s += step;
+				iBegin += n;
+			}
+			sBegin += step;
+		}
+
+		return embedSignal;
+	}
+	*/
 
 }
