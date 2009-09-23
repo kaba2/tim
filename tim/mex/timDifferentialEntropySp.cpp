234c7581503c60a769d021e45d7bac1ab3634440
kaba <kaba@localhost>
2009-09-23 17:09:11 +0300

    Added missing files for the Sp-estimator.

---
+++ tim/mex/timDifferentialEntropySp.cpp
@@ -0,0 +1,52 @@
+#include "mex.h"
+
+#include "tim/core/differential_entropy_sp.h"
+
+#include <boost/static_assert.hpp>
+
+#include <pastel/sys/pastelomp.h>
+
+using namespace Tim;
+
+void mexFunction(int outputs, mxArray *outputSet[],
+				 int inputs, const mxArray *inputSet[])
+{
+	enum
+	{
+		RealIsDouble = boost::is_same<real, double>::value
+	};
+	BOOST_STATIC_ASSERT(RealIsDouble);
+
+	enum
+	{
+		xIndex
+	};
+
+	const integer trials = mxGetNumberOfElements(inputSet[xIndex]);
+
+	std::vector<SignalPtr> xEnsemble;
+	xEnsemble.reserve(trials);
+
+	for (integer i = 0;i < trials;++i)
+	{
+		mxArray* signalArray = mxGetCell(inputSet[xIndex], i);
+
+		// It is intentional to assign the width
+		// and height the wrong way. The reason
+		// is that Matlab uses column-major storage
+		// while we use row-major storage.
+		const integer samples = mxGetN(signalArray);
+		const integer dimension = mxGetM(signalArray);
+
+		real* rawData = mxGetPr(signalArray);
+
+		xEnsemble.push_back(SignalPtr(
+			new Signal(samples, dimension, rawData)));
+	}
+
+	outputSet[0] = mxCreateDoubleMatrix(1, 1, mxREAL);
+	real* rawResult = mxGetPr(outputSet[0]);
+
+	*rawResult = differentialEntropySp(
+		forwardRange(xEnsemble.begin(), xEnsemble.end()));
+}
