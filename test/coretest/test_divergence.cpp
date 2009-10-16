dbd0397f89ee9325336272c71a175429ec7f1190
kaba <kaba@localhost>
2009-10-17 00:56:38 +0300

    Added a missing file.

---
+++ test/coretest/test_divergence.cpp
@@ -0,0 +1,31 @@
+#include "estimation.h"
+
+#include "tim/core/signal_tools.h"
+#include "tim/core/embed.h"
+
+#include <pastel/sys/random.h>
+
+#include "tim/core/divergence_wkv.h"
+
+using namespace Tim;
+
+namespace
+{
+
+	void testDivergence()
+	{
+		SignalPtr xSignal = generateGaussian(10000, 10);
+		SignalPtr ySignal = generateGaussian(10000, 10);
+
+		const real div = divergenceWkv(xSignal, ySignal);
+		log() << "Divergence = " << div << logNewLine;
+	}
+
+	void addTest()
+	{
+		timTestList().add("Divergence", testDivergence);
+	}
+
+	CallFunction run(addTest);
+
+}
