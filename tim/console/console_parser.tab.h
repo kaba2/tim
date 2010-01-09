564f288dfdd413dba998d17cdfe3b7de161ecde6
kaba <kaba@localhost>
2010-01-09 13:20:20 +0200

    Updated TIM Console demo files.

---
+++ tim/console/console_parser.tab.h
@@ -0,0 +1,103 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     T_INTEGER = 258,
+     T_REAL = 259,
+     T_IDENTIFIER = 260,
+     T_PRINT = 261,
+     T_STRING = 262
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1676 of yacc.c  */
+#line 53 "console_parser.y"
+
+	Tim::Statement_AstNode* statement;
+	Tim::Expression_AstNode* expression;
+	Tim::Declaration_AstNode* declaration;
+	Tim::FunctionCall_AstNode* function_call;
+	Tim::CellArray_AstNode* cell_array;
+	Tim::RealArray_AstNode* real_array;
+	Tim::StatementSet* program;
+	
+	Tim::StringSet* cell_list;
+	Tim::StringSetSet* cell_array_content;
+	Tim::RealSet* real_list;
+	Tim::RealSetSet* real_array_content;
+	Tim::ExpressionSet* expression_list;
+	Tim::real real_value;
+	Tim::integer integer_value;
+
+	std::string* string;
+
+
+
+/* Line 1676 of yacc.c  */
+#line 81 "console_parser.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE console_lval;
+
+#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
+typedef struct YYLTYPE
+{
+  int first_line;
+  int first_column;
+  int last_line;
+  int last_column;
+} YYLTYPE;
+# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+# define YYLTYPE_IS_DECLARED 1
+# define YYLTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYLTYPE console_lloc;
+
