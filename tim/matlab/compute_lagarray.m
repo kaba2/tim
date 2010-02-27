e1feff4f164164bb11d00b4daa0a21ff77663aa8
Kalle Rutanen <kaba@hilvi.org>
2010-02-27 17:47:13 +0200

    Updated documentation.

--- tim/matlab/compute_lagarray.m
+++ tim/matlab/compute_lagarray.m
@@ -1,3 +1,25 @@
+% COMPUTE_LAGARRAY
+% Computes a lag-array from a lag set.
+%
+% L = compute_lagarray(lagSet)
+%
+% where
+%
+% LAGSET is an arbitrary dimensional cell-array
+% containing either real arrays or scalars.
+% All the real arrays that are not scalars must
+% have the same number M of elements. The LAGSET
+% is treated by its linearization, as are the
+% contained arrays. The scalars will be extended
+% to arrays of the size M with the same scalar
+% in all elements.
+%
+% L is a 2-dimensional real array, where L(i, :)
+% contains M lags corresponding to LAGSET(i).
+
+% Description: Computes a lag-array from a lag set
+% Documentation: tim_matlab_matlab.txt
+
 function L = compute_lagarray(lagSet)
 
 lagArrays = numel(lagSet);
