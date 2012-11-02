7f1a145c194ed2d6269afc2ecd6b51f3bba3ac55
kaba <kaba@localhost>
2012-11-02 21:41:39 +0200

    Added version number function.

---
+++ tim/matlab/tim_version.m
@@ -0,0 +1,18 @@
+% TIM_VERSION
+% TIM version number
+%
+% x = tim_version()
+%
+% where
+%
+% X is a string specifying the version number in the form
+% x.y.z, where x, y, and z are integers. The x specifies the
+% major version number, the y specifies the minor version
+% number, and the z specifies the bug-fix version number.
+
+% Description: TIM version number
+% Documentation: support_functions.txt
+
+function x = tim_version()
+
+x = '1.3.0';
