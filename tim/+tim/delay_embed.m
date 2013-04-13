d46691330f7f19fa4b385581d569897376e5b057
kaba <kaba@localhost>
2013-04-13 15:04:50 +0300

    Added embedding distance ratio.

--- tim/+tim/delay_embed.m
+++ tim/+tim/delay_embed.m
@@ -1,20 +1,20 @@
 % DELAY_EMBED 
 % Delay-embeds a signal.
 %
-% Y = delay_embed(X, k, dt)
+% Y = delay_embed(X, k, tDelta)
 %
 % where
 %
 % K is the "embedding factor".
 %
-% DT is the embedding delay.
+% TDELTA is the embedding delay.
 
 % Description: Delay-embedding
 % Documentation: delay_embed.txt
 
-function Y = delay_embed(X, k, dt)
+function Y = delay_embed(X, k, tDelta)
 
-if nargin < 3 || isempty(dt), dt = 1; end
+if nargin < 3 || isempty(tDelta), tDelta = 1; end
 if nargin < 2 || isempty(k) || isempty(X),
     error('Not enough input arguments');
 end
@@ -24,7 +24,7 @@ end
 if iscell(X),
     Y = cell(size(X));
     for i = 1:length(X)
-        Y{i} = delay_embed(X{i}, k, dt);        
+        Y{i} = delay_embed(X{i}, k, tDelta);        
     end
     return;
 end
@@ -33,25 +33,25 @@ if k < 0,
     error('k must be a non-negative integer.');
 end
 
-if dt < 1,
-    error('dt must be a positive integer.');
+if tDelta < 1,
+    error('tDelta must be a positive integer.');
 end
 
 % use this line when TIM estimators admit NaNs
-X = [nan(size(X, 1), (k - 1) * dt), X];
+X = [nan(size(X, 1), (k - 1) * tDelta), X];
 % for the moment we use circular shifting
-%tmp = circshift(X, [0 (k-1)*dt]);
-%X = [tmp(:,1:(k-1)*dt) X];
+%tmp = circshift(X, [0 (k-1)*tDelta]);
+%X = [tmp(:,1:(k-1)*tDelta) X];
 
 n = size(X, 1);
 
 embedDimension = k * n;
-embedSampleWidth = (k - 1) * dt + 1;
+embedSampleWidth = (k - 1) * tDelta + 1;
 embedSamples = size(X, 2) - embedSampleWidth + 1;
 
 Y = zeros(embedDimension, embedSamples);
 
 for j = 1 : k
-   s = dt * (j - 1) + 1; 
+   s = tDelta * (j - 1) + 1; 
    Y((j - 1) * n + 1 : j * n, :) = X(:, s : (s + embedSamples - 1));
 end
---
+++ tim/+tim/embedding_distance_ratio.m
@@ -0,0 +1,41 @@
+function distanceRatioSet = embedding_distance_ratio(pointSet, varargin)
+
+import([tim_package, '.*']);
+
+concept_check(nargin, 'inputs', 1);
+concept_check(nargout, 'outputs', 0 : 1);
+
+% Optional input arguments.
+
+embeddingFactorSet = 1 : 16;
+eval(process_options({'embeddingFactorSet'}, varargin));
+
+d = size(pointSet, 1);
+n = size(pointSet, 2);
+
+embeddingFactors = numel(embeddingFactorSet);
+
+distanceRatioSet = zeros(embeddingFactors, n);
+for i = 1 : embeddingFactors
+    embeddingFactor = embeddingFactorSet(i);
+    
+    lowerPointSet = tim.delay_embed(pointSet, 1, 1);
+    higherPointSet = tim.delay_embed(pointSet, embeddingFactor + 1, 1);
+
+    kdTree = pastelgeometry.PointKdTree(d);
+    idSet = kdTree.insert(lowerPointSet);
+    kdTree.refine();
+
+    neighborSet = kdTree.search_nearest(idSet, ...
+        ones(1, n) * Inf, 1);
+
+    aIdSet = idSet(neighborSet > 0);
+    bIdSet = neighborSet(neighborSet > 0);
+
+    lowerDistanceSet = sqrt(sum(...
+        (lowerPointSet(:, aIdSet) - lowerPointSet(:, bIdSet)).^2));
+    higherDistanceSet = sqrt(sum(...
+        (higherPointSet(:, aIdSet) - higherPointSet(:, bIdSet)).^2));
+    
+    distanceRatioSet(i, :) = higherDistanceSet / lowerDistanceSet;
+end
