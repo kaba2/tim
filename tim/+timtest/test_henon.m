ee36a20583d382b834cbeff2d37b2567319b4999
kaba <kaba@localhost>
2013-04-13 18:36:36 +0300

    Updated tests.

--- tim/+timtest/test_henon.m
+++ tim/+timtest/test_henon.m
@@ -3,5 +3,16 @@ close all;
 
 pointSet = tim.henon();
 
-timtest.visualize_embedding(pointSet);
+% The first minimum of auto mutual information fails to
+% predict a good embedding lag for this data set.
+% The reason is that the Henon map lacks continuity, an 
+% assumption of the automated method. The best embedding lag 
+% is empirically found to be 1 (just try different choices).
+% I suspect this is a general property of an iterated
+% function; every step is important.
+
+% The auto mutual information goes to zero almost immediately; 
+% again I suspect this is a general property of an iterated function.
+
+timtest.visualize_embedding(pointSet, 'tDelta', 1);
 
