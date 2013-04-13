e788365375e877cf342568ffa4a6604121930436
Kalle Rutanen <kaba@hilvi.org>
2013-04-13 16:39:18 +0300

    Added tests.

---
+++ tim/+test-tim/test_ex2_1.m
@@ -0,0 +1,21 @@
+% Non-linear time series analysis
+% Exercise 2.1
+
+clear all;
+close all;
+
+n = 1024;
+
+dataSet = randn(1, n);
+visualize(dataSet);
+
+% Take a moving-average. This should cut down
+% on the high frequencies.
+radius = 7;
+filteredSet = zeros(1, n);
+for i = 1 : n
+    filteredSet(i) = mean(dataSet(1, ...
+        max(i - radius, 1) : min(i + radius, n)));
+end
+
+visualize(filteredSet);
