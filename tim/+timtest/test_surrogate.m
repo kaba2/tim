64e436b8465562fe5250446c720c5d3775dd5cf9
kaba <kaba@localhost>
2013-06-07 15:33:11 +0200

    Added dynamics-preserving surrogates.

--- tim/+timtest/test_surrogate.m
+++ tim/+timtest/test_surrogate.m
@@ -1,17 +1,25 @@
 clear all;
 close all;
 
-n = 63;
-filterOrder = 16;
+n = 200;
+filterOrder = 21;
 
 dataSet = randn(1, n);
 filterSet = fir1(filterOrder, 0.5);
 filteredSet = filter(filterSet, 1, dataSet);
 surrogateSet = tim.surrogate(filteredSet, ...
-	'frequency_range', [0.4, 1]);
+	'frequency_range', [0.4, 1], ...
+	'algorithm', 'preserve_dynamics', ...
+	'k', 1);
 
 figure;
-hold all;
-plot(filteredSet);
-plot(surrogateSet);
+hold on;
+area(filteredSet);
+plot(surrogateSet, 'r');
+hold off;
+
+figure;
+hold on;
+area(fftshift(abs(fft(filteredSet))));
+plot(fftshift(abs(fft(surrogateSet))), 'r');
 hold off;
