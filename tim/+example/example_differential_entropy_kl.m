e5e4b992ca0d51ff1d27b13a0335240cc5753e3f
kaba <kaba@localhost>
2013-10-22 22:27:57 +0300

    Updated documentation.

--- tim/+example/example_differential_entropy_kl.m
+++ tim/+example/example_differential_entropy_kl.m
@@ -1,5 +1,15 @@
+% Description: Visualization of bias in differential_entropy_kl
+
 clear all;
 close all;
+
+% This example demonstrates numerically that the 
+% differential_entropy_kl estimator is biased. 
+% Further:
+%
+% 1) as d increases, the bias increases,
+% 2) as k increases, the bias increases,
+% 3) as k increases, the variance decreases.
 
 for i = 0 : 2
     for j = 0 : 2
