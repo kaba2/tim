6ac3b2fa8eef9c61b0774ef8f1b2c4f206649d8d
kaba <kaba@localhost>
2014-10-15 00:18:17 +0300

    Updated build-system for bitness and automatic directories.

--- tim/build_tim.m
+++ tim/build_tim.m
@@ -1,43 +1,106 @@
 % BUILD_TIM
-% Builds a Matlab mex-library for TIM.
+% Builds TIM Matlab mex-library.
 %
-% build_pastel(mode)
+% build_tim()
+% build_tim('key', value, ...)
 %
-% where
+% Optional arguments
+% ------------------
 %
-% MODE is a string which specifies the configuration to use for building TIM. 
-% It must be one of 
+% MODE ('mode') is a string which specifies the configuration to use for 
+% building the TIM library. It must be one of 
 %     debug
 %     release
 %     relwithdebinfo
 % Default: release
+%
+% VERBOSE ('verbose') is a string which specifies whether to print 
+% additional information about the build process. Must be either 'on' 
+% or 'off'.
+% Default: off
 
 % Description: Builds a Matlab mex-library for TIM.
 % Documentation: building.txt
 
-function build_tim(mode)
+function build_tim(varargin)
 
-if nargin < 1
-    mode = 'release';
-end
+% Path to Pastel.
+pastelPath = '../../pastel';
+
+% Path to Boost.
+boostPath = '../../boost_1_56_0';
+
+% Path to Threading Building Blocks.
+tbbPath = '../../tbb42';
+
+% Add Pastel to Matlab path so that
+% process_options is available.
+addpath([pastelPath, '/pastel']);
+
+% Optional input arguments
+mode = 'release';
+verbose = 'off';
+eval(pastelsys.process_options(...
+    {'mode', 'verbose'}, ...
+    varargin));
 
 modeSet = {'debug', 'release', 'relwithdebinfo'};
 if ~ismember(mode, modeSet)
-    error(['MODE must be either debug, release, or relwithdebinfo.']);
+    error(['MODE must be one of debug, release, or relwithdebinfo.']);
+end
+
+verboseSet = {'on', 'off'};
+if ~ismember(verbose, verboseSet)
+    error(['VERBOSE must be one on or off.']);
+end
+
+% Determine the bitness of the running Matlab version
+% (not of the operating system or the computer).
+[ignore, maxArraySize] = computer;
+bits = 32;
+if maxArraySize >= 2^32
+    bits = 64;
 end
 
 disp(' ');
-disp(['Building a mex file for TIM in ', mode, ' mode.']);
+disp(['Building a mex file for TIM in ', ...
+    num2str(bits), '-bit ', ...
+    mode, ' mode.']);
 disp(' ');
 
-timIncludePath = '..';
-pastelIncludePath = '../../pastel';
-boostIncludePath = '../../boost';
-tbbIncludePath = '../../tbb42/include';
+% Compute the actual paths.
 
-timLibraryPath = ['../lib/', mode];
-pastelLibraryPath = ['../../pastel/lib/', mode];
-tbbLibraryPath = ['../../tbb42/lib/intel64/vc11'];
+pastelIncludePath = pastelPath;
+pastelLibraryPath = [...
+    '../../pastel/lib', ...
+    '/', num2str(bits), ...
+    '/', mode];
+
+timIncludePath = '..';
+timLibraryPath = [...
+    '../lib', ...
+    '/', num2str(bits), ...
+    '/', mode];
+
+boostIncludePath = boostPath;
+
+tbbIncludePath = [tbbPath, '/include'];
+tbbLibraryPath = [tbbPath, '/lib'];
+
+if ispc()
+    % Windows
+    if bits == 64
+        tbbLibraryPath = [tbbLibraryPath, '/intel64/vc11'];
+    else
+        tbbLibraryPath = [tbbLibraryPath, '/ia32/vc11'];
+    end
+elseif ismac()
+    % Mac Os X
+    tbbLibraryPath = [tbbLibraryPath, '/libc++'];
+else
+    % Linux and others
+    tbbLibraryPath = [tbbLibraryPath, ''];
+end
 
 inputPath = ['matlab'];
 outputPath = ['+tim'];
@@ -111,6 +174,10 @@ end
 % Other flags.
 if strcmp(mode, 'debug')
     commandSet{end + 1} = ' -g';
+end
+
+if strcmp(verbose, 'on')
+    commandSet{end + 1} = ' -v';
 end
 
 % Run the build-command
