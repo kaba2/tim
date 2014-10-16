54faae4b74a38d7bd8bbfad1e18ab548d317e661
Kalle Rutanen <kaba@hilvi.org>
2014-10-16 19:58:45 +0300

    Updated example.

--- tim/+tim/differential_entropy_sine.m
+++ tim/+tim/differential_entropy_sine.m
@@ -1,18 +1,41 @@
 % DIFFERENTIAL_ENTROPY_SINE
-% Differential entropy of Y = sin(2 * pi * X), where X ~ Uniform(0, 1).
+% Differential entropy of Y = sin(2 * pi * X + beta), X ~ Uniform(0, 1).
 %
 % H = differential_entropy_sine()
+%
+% Here BETA is a real number. The differential entropy of Y is 
+% not dependent of BETA.
 %
 % The cumulative distribution function of Y is 
 % F : [-1, 1] --> [0, 1] such that
 %
-%     F(y) = [asin(y) + (pi / 2)] / pi
+%     F(y) = [max(asin(y) + (pi / 2) - beta, 0) + 
+%             max(asin(y) - (pi / 2) + beta, 0)] / pi
+%
+% Let 
+%
+%     a = sin(beta - pi / 2)
+%     b = sin(pi / 2 - beta)
 %
 % The probability density function of Y is
-% f : [-1, 1] --> R such that
+% f : [-1, 1] --> R such that 
 %
-%     f(y) = 1 / (pi sqrt(1 - y^2)).
+%     f(y) = 2 / (pi sqrt(1 - y^2)),    a <= y and b <= y,
+%            1 / (pi sqrt(1 - y^2)),    a <= y xor b <= y,
+%            0,                         a > y and b > y,
+%
+% The differential entropy of Y can then be solved as
+%
+%     H(Y) = log2(pi) - 1.
+
+% Description: Differential entropy of Y = sin(2 * pi * X + beta), X ~ Uniform(0, 1).
+% Documentation: differential_entropy.txt
 
 function H = differential_entropy_sine()
+
+import([tim_package, '.*']);
+
+concept_check(nargin, 'inputs', 0);
+concept_check(nargout, 'outputs', 0 : 1);
 
 H = log2(pi) - 1;
