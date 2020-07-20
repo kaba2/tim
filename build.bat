b416caf601ee0b99dbc041f91f384e62a6c3a7dd
kaba <kaba@hilvi.org>
2020-07-20 03:25:51 +0300

    Updated gitignore.

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+if not exist build (
+    mkdir build
+)
+
+if exist "build\CMakeCache.txt" (
+    del "build\CMakeCache.txt"
+)
+cmake -S . -G"Visual Studio 16 2019" -T "ClangCL" -DCMAKE_BUILD_TYPE=Release -B build "-DCMAKE_TOOLCHAIN_FILE=C:\code\vcpkg\scripts\buildsystems\vcpkg.cmake"
+REM cmake -S . -G"Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -B build "-DCMAKE_TOOLCHAIN_FILE=C:\code\vcpkg\scripts\buildsystems\vcpkg.cmake"
+
+REM start build\Tim2.sln
