920dccb76ebe0e092c4145090a50e98bed202077
kaba <kaba@hilvi.org>
2020-07-20 03:47:35 +0300

    Updated build system.

--- matlab/build_tim.template.m
+++ matlab/build_tim.template.m
@@ -7,13 +7,6 @@
 % Optional arguments
 % ------------------
 %
-% MODE ('mode') is a string which specifies the configuration to use for 
-% building the Tim sub-library. It must be one of 
-%     debug
-%     release
-%     relwithdebinfo
-% Default: release
-%
 % VERBOSE ('verbose') is a string which specifies whether to print 
 % additional information about the build process. Must be either 'on' 
 % or 'off'.
@@ -26,12 +19,12 @@ function build_tim(varargin)
 
 % Add Pastel to Matlab path so that
 % process_options is available.
-addpath(['${PastelDirectory}', '/matlab']);
+addpath(['${PastelExecutableDirectory}', '/matlab']);
 
 % Optional input arguments
-mode = 'release';
+mode = '${LOWER_CMAKE_BUILD_TYPE}';
 verbose = 'off';
-eval(pastelsys.process_options(...
+eval(pastelmatlab.process_options(...
     {'libraryName', 'mode', 'verbose'}, ...
     varargin));
 
@@ -41,14 +34,6 @@ eval(pastelsys.process_options(...
 bits = 32;
 if maxArraySize >= 2^32
     bits = 64;
-end
-
-generatedBits = ${GENERATOR_BITS};
-if bits ~= generatedBits
-    error(['Build file is ', num2str(generatedBits), '-bit, but ', ...
-        'Matlab is ', num2str(bits), '-bit. ', ...
-        'Switch to ', num2str(bits), '-bit Matlab, or rerun Tim CMake with a ', ...
-        num2str(bits), '-bit generator.'])
 end
 
 modeSet = {'debug', 'release', 'relwithdebinfo'};
@@ -62,14 +47,7 @@ if ~ismember(verbose, verboseSet)
 end
 
 libraryName = 'core';
-
-disp(' ');
-disp(['Building a mex file for ', ...
-    'tim', libraryName, 'matlab', ...
-    ' in ', ...
-    num2str(bits), '-bit ', ...
-    mode, ' mode.']);
-disp(' ');
+completeLibraryName = ['tim_matlab'];
 
 inputDirectory = ['${TimDirectory}/tim/', libraryName, 'matlab'];
 outputDirectory = '+tim';
@@ -82,161 +60,48 @@ includeDirectorySet = ...
 {...
     '${TimIncludeDirectory}', ...
     '${PastelIncludeDirectory}', ...
-    '${BoostIncludeDirectory}', ...
+    '${RangesIncludeDirectory}', ...
+    '${Boost_INCLUDE_DIRS}', ...
     '${TbbIncludeDirectory}', ...
-    '${ArmadilloIncludeDirectory}'...
+    '${EIGEN3_INCLUDE_DIRS}'...
 };
 
 libraryDirectorySet = ...
 {...
-    ['${PastelLibraryDirectory}/', mode], ...
-    ['${TimLibraryDirectory}/', mode], ...
-    '${TbbLibraryDirectory}', ...
-    '${ArmadilloLibraryDirectory}', ...
+    '${PastelLibraryDirectory}', ...
+    '${TimLibraryDirectory}', ...
+    '${TbbLibraryDirectory}' ...
 };
-
-if ~ismac()
-    libraryDirectorySet = ...
-    [...
-        libraryDirectorySet, ...
-        '${LapackLibraryDirectory}', ...
-        '${BlasLibraryDirectory}'
-    ];
-end
 
 % Libraries
 
 librarySet = ...
 {...
-    '${TbbLibraryName}', ...
-    '${ArmadilloLibraryName}' ...
+    '${TbbLibraryName}' ...
 };
-
-if ~ismac()
-    librarySet = ...
-    [...
-        librarySet, ...
-        '${BlasLibraryName}', ...
-        '${LapackLibraryName}' ...
-    ];
-end
 
 if strcmp(libraryName, 'core')
     librarySet = [...
         librarySet, ...
         'timcorematlab', ...
         'timcore', ...
-        'pastelgeometry', ...
-        'pastelmath', ...
-        'pastelmatlab', ...
-        'pastelsys' ...
+        'pastel', ...
+        'pastelmatlab' ...
     ];
 end
 
-% Handle semicolon-separated lists
-% --------------------------------
+fileSet = {[inputDirectory, '/tim_matlab.cpp']};
 
-function resultSet = breakSemicolons(stringSet)
-    resultSet = {};
-    for i = 1 : numel(stringSet)
-        string = stringSet{i};
-        splitSet = regexp(string, ';', 'split');
-        resultSet = [resultSet, splitSet];
-    end
-end
-
-includeDirectorySet = breakSemicolons(includeDirectorySet);
-libraryDirectorySet = breakSemicolons(libraryDirectorySet);
-librarySet = breakSemicolons(librarySet);
-
-% Preprocessor definitions 
-% ------------------------
-
-defineSet{end + 1} = '_ITERATOR_DEBUG_LEVEL=0';
-
-% Form the build-command
-% ----------------------
-
-commandSet = {};
-commandSet{end + 1} = 'mex';
-commandSet{end + 1} = [' ', inputDirectory, '/tim_matlab.cpp'];
-
-% Add preprocessor definitions.
-for i = 1 : numel(defineSet)
-    commandSet{end + 1} = [' -D', defineSet{i}];
-end
-
-% Add include paths.
-for i = 1 : numel(includeDirectorySet)
-    commandSet{end + 1} = [' -I''', includeDirectorySet{i}, ''''];
-end
-
-% Add library directories.
-for i = 1 : numel(libraryDirectorySet)
-    libraryDirectory = libraryDirectorySet{i};
-    if isempty(libraryDirectory)
-        % No directory was specified; skip it.
-        continue;
-    end
-
-    [iStart, iEnd] = regexp(libraryDirectory, '\.framework$');
-    if ~isempty(iStart) && ismac()
-        % This is a framework directory.
-        commandSet{end + 1} = [' LDFLAGS=''$LDFLAGS -F ', libraryDirectory, ''''];
-    else
-        % This is a normal library directory.
-        commandSet{end + 1} = [' -L''', libraryDirectory, ''''];
-    end
-end
-
-% Add output path.
-commandSet{end + 1} = [' -outdir ''', outputDirectory, ''''];
-
-% Add libraries.
-for i = 1 : numel(librarySet)
-    library = librarySet{i};
-    if isempty(library)
-        % No library was specified; skip it.
-        continue;
-    end
-
-    [iStart, iEnd] = regexp(library, '\.framework$');
-    if ~isempty(iStart) && ismac()
-        % This is a framework (Mac OS X).
-        
-        % Get the framework name by stripping out the
-        % trailing '.framework'.
-        frameworkName = library(1 : iStart - 1);
-
-        % Mex does not have a direct flag to specify
-        % frameworks. Instead, we embed it directly
-        % into compiler flags.
-        commandSet{end + 1} = [' CXXLIBS=''$CXXLIBS -framework ', frameworkName, ''''];
-    else
-        % This is a normal library.
-        commandSet{end + 1} = [' -l', librarySet{i}];
-    end
-end
-
-% Other flags.
-if strcmp(mode, 'debug')
-    commandSet{end + 1} = ' -g';
-end
-
-if strcmp(verbose, 'on')
-    commandSet{end + 1} = ' -v';
-end
-
-% Run the build-command
-% ---------------------
-
-% Print the executed build-command in pretty form.
-for i = 1 : numel(commandSet)
-    disp(commandSet{i});
-end
-
-% Run the command.
-buildCommand = [commandSet{:}];
-eval(buildCommand);
+commandSet = form_build_command(...
+    fileSet, ...
+    'outputLibraryName', completeLibraryName, ...
+    'includeDirectorySet', includeDirectorySet, ...
+    'libraryDirectorySet', libraryDirectorySet, ...
+    'librarySet', librarySet, ...
+    'defineSet', defineSet, ...
+    'outputDirectory', outputDirectory, ...
+    'mode', mode, ...
+    'verbose', verbose, ...
+    'run', true);
 
 end
