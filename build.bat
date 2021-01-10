af499a5d7f559b8b3f04d05a1803ff192c7f66a1
kaba <kaba@hilvi.org>
2021-01-11 01:10:24 +0200

    Updated build system.

--- build.bat
+++ build.bat
@@ -1,13 +1,32 @@
 @echo off
 
-if not exist build (
-    mkdir build
+set buildDir=build
+if not exist %buildDir% (
+    echo Creating directory %buildDir%/...
+    mkdir %buildDir%
 )
 
-if exist "build\CMakeCache.txt" (
-    del "build\CMakeCache.txt"
+set buildType=RelWithDebInfo
+if NOT [%1]==[] (
+    set buildType=%1
 )
-REM cmake -S . -G"Visual Studio 16 2019" -T "ClangCL" -DCMAKE_BUILD_TYPE=RelWithDebInfo -B build "-DCMAKE_TOOLCHAIN_FILE=C:\code\vcpkg\scripts\buildsystems\vcpkg.cmake"
-cmake -S . -G"Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=RelWithDebInfo -B build "-DCMAKE_TOOLCHAIN_FILE=C:\code\vcpkg\scripts\buildsystems\vcpkg.cmake"
 
-REM start build\Tim.sln
+set toolchainFile="C:\code\vcpkg\scripts\buildsystems\vcpkg.cmake"
+
+if exist "%buildDir%\CMakeCache.txt" (
+    echo Removing existing CMakeCache.txt...
+    del "%buildDir%\CMakeCache.txt"
+)
+
+echo Type: %buildType%
+echo Directory: %buildDir%
+echo Toolchain: %toolchainFile%
+
+REM goto NARP
+
+REM cmake -S . -G"Visual Studio 16 2019" -T ClangCL -DCMAKE_BUILD_TYPE=%buildType% -B %buildDir% -DCMAKE_TOOLCHAIN_FILE=%toolchainFile%
+cmake -S . -G"Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=%buildType% -B %buildDir% -DCMAKE_TOOLCHAIN_FILE=%toolchainFile%
+
+REM start %buildDir%\Tim.sln
+
+:NARP
